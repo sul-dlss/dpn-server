be2aacb53f5a46aeefeb7ad3c0e045fbcf764170
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2016-08-29 10:23:23 -0700

    WIP to fix migrations on MySQL - 2

--- db/migrate/20160429211031_change_fixity_checks_to_digests.rb
+++ db/migrate/20160429211031_change_fixity_checks_to_digests.rb
@@ -4,33 +4,8 @@
 # See LICENSE.md for details.
 
 class ChangeFixityChecksToDigests < ActiveRecord::Migration
-
-  class Bag < ActiveRecord::Base
-  end
-
-  class FixityCheck < ActiveRecord::Base
-    belongs_to :bag
-  end
-
-  def up
-    add_foreign_key :fixity_checks, :nodes,
-      column: :node_id,
-      on_delete: :nullify,
-      on_update: :cascade
-
-    FixityCheck.all.each do |fc|
-      fc.update!(node_id: fc.bag.admin_node_id)
-    end
-
-    add_column :fixity_checks, :created_at, :datetime
-    change_column :fixity_checks, :created_at, :datetime, null: false
-
+  def change
+    add_reference :fixity_checks, :node, index: true, foreign_key: true
     rename_table :fixity_checks, :message_digests
-  end
-
-  def down
-    rename_table :message_digests, :fixity_checks
-    remove_column :fixity_checks, :created_at
-    remove_foreign_key :fixity_checks, :node_id
   end
 end

fd8fd43fb25792a3283b48f687914db3615ada74
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2016-09-13 16:23:58 -0700

    git submodule support with capistrano/git-submodule-strategy

--- Capfile
+++ Capfile
@@ -7,6 +7,7 @@ require 'dlss/capistrano'
 require 'capistrano/bundler'
 require 'capistrano/rails'
 require 'capistrano/passenger'
+require 'capistrano/git-submodule-strategy'
 
 # Load custom tasks from `lib/capistrano/tasks` if you have any defined
 Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -2,16 +2,14 @@
 lock '3.6.1'
 
 set :application, 'dpn-server'
-set :repo_url, 'https://github.com/sul-dlss/dpn-server.git'
-
-# Default branch is :master
-ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/opt/app/dpn/dpn-server'
 
-# Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
+set :git_strategy, Capistrano::Git::SubmoduleStrategy
+set :repo_url, 'https://github.com/sul-dlss/dpn-server.git'
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default value for :format is :pretty
 # set :format, :pretty
